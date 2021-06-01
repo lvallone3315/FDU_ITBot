1886a0ed25139466728503a98e6dc41b5605f18f
Lee Vallone <lvallone3315@gmail.com>
2021-06-01 16:55:45 -0400

    v0.2 continued - recovered main() changes, need to brush up on my "stash"

--- src/fdu_itbot/FDU_ITBot.java
+++ src/fdu_itbot/FDU_ITBot.java
@@ -14,16 +14,22 @@ public class FDU_ITBot {
      * Instantiates & calls UI to manage input & output
      */
     
-    private final static String VERSION = "v0.1";
+    private final static String VERSION = "v0.2";
     private final static String WELCOME_MESSAGE = "Welcome to the FDU IT bot.\n  What is your name? ";
     private final static String INITIAL_QUERY_PART1 = "Hi ";
     private final static String INITIAL_QUERY_PART2 = "! How can I help you?";
+    private final static String NEXT_QUERY = "Please specify a help topic (type exit to quit)";
     
     private static String userName;
     
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        String userInput;
+
         FDU_ITBot_UI ITBot_UI = new FDU_ITBot_UI();
+        FDU_ITBot_ParseUserInput ITBot_Parser = new FDU_ITBot_ParseUserInput();
+        FDU_ITBot_DataRepo dataRepo = new FDU_ITBot_DataRepo();
+        
         ITBot_UI.outputToUser("FDU IT Bot " + VERSION + "\n\n");
         ITBot_UI.outputToUser(WELCOME_MESSAGE);
         
@@ -38,6 +44,17 @@ public class FDU_ITBot {
             userName = inputtedName;
         }
         ITBot_UI.outputToUser(INITIAL_QUERY_PART1 + userName + INITIAL_QUERY_PART2);
+        
+        // loop until user types "exit"
+        do {
+            userInput = ITBot_UI.getInputFromUser();
+            String parsedInput = ITBot_Parser.parseInput(userInput);
+            ITBot_UI.outputToUser("You typed ... " + parsedInput);
+            int answerID = dataRepo.searchByKeyword(parsedInput);
+            if (answerID != FDU_ITBot_DataRepo.NO_RESULT)
+                ITBot_UI.outputToUser(dataRepo.getAnswer(answerID));
+            ITBot_UI.outputToUser(NEXT_QUERY);
+        } while (!userInput.equals("exit"));
         
         try {  // wait before exiting - so user can see screen
             Thread.sleep(1000);
